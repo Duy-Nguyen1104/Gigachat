6b6159da94f85417e91480ff24572df98292b2af
Duy Nguyen <duyng2311@gmail.com>
2026-05-27 15:42:09 +1000

    fix: use aws access and secret key instead of profile

--- src/main/java/com/project/gigachat/config/S3Config.java
+++ src/main/java/com/project/gigachat/config/S3Config.java
@@ -3,35 +3,29 @@ package com.project.gigachat.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 /**
- * AWS S3 configuration.
- *
- * Credentials selection:
- * - If AWS_PROFILE (or AWS_DEFAULT_PROFILE) is set, it will use that profile.
- * - Otherwise, it falls back to DefaultCredentialsProvider, which checks
- *   environment variables, system properties, default profile, etc.
+ * AWS S3 configuration using DefaultCredentialsProvider.
+ * This will automatically use credentials from:
+ * - Environment variables (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+ * - AWS credentials file (~/.aws/credentials)
+ * - IAM role (if running on EC2)
  */
 @Configuration
 public class S3Config {
     
     @Value("${aws.s3.region}")
     private String region;
-
-    @Value("${AWS_PROFILE:gigachat-user}")
-    private String awsProfile;
     
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(credentialsProvider())
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
     
@@ -39,18 +33,7 @@ public class S3Config {
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(credentialsProvider())
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
-
-    private AwsCredentialsProvider credentialsProvider() {
-        // Prefer an explicit profile if the env var is set to something other than "default".
-        if (awsProfile != null && !awsProfile.isBlank() && !"default".equals(awsProfile)) {
-            return ProfileCredentialsProvider.builder()
-                    .profileName(awsProfile)
-                    .build();
-        }
-        return DefaultCredentialsProvider.create();
-    }
 }
-
